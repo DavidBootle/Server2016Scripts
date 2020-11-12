f396d345d9a5556f249cd147381bdedb1003a0d9
David Bootle <d.bootle18@psaschool.org>
2020-11-12 12:26:16 -0500

    removed unnessecary quotes

--- setup_scripts.bat
+++ setup_scripts.bat
@@ -1,5 +1,5 @@
 @echo off
 set /p directory="Enter the absolute directory of the folder containing the scripts: "
 setx SCRIPTDIR %directory%
-echo "Script directory variable has been set to '%directory%'. Access the variable with %%SCRIPTDIR%%."
+echo Script directory variable has been set to '%directory%'. Access the variable with %%SCRIPTDIR%%.
 pause
