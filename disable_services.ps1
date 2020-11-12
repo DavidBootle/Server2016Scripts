48634a16af048326f33e96bfbec49f6c830c1657
David Bootle <d.bootle18@psaschool.org>
2020-11-12 10:17:00 -0500

    updated to work properly

--- disable_services.ps1
+++ disable_services.ps1
@@ -14,14 +14,25 @@ $crit_services = $temp
 
 # ftp
 if ($crit_services -notcontains "FTP") {
-    # TODO finish this
-    Disable-WindowsOptionalFeature -Online IIS-FTPServer
-    Write-Output "Disabled FTP Server"
+    try {
+        Disable-WindowsOptionalFeature -Online -FeatureName IIS-FTPServer
+        Stop-Service 'Server' -Force -Confirm
+        Write-Output "Disabled FTP Server"
+    }
+    catch {
+        Write-Output "Failed to disable FTP Server"
+    }
+    
 }
 
 # rpd
 if ($crit_services -notcontains "RDP") {
-    Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 1 # sets the actual toggle value to turn off rdp in the "allow rdp" menu
-    Disable-WindowsOptionalFeature -Online -FeatureName Remote-Desktop-Services #turns off the actual service
-    Write-Output "Disabled RDP"
+    try {
+        Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 1 # sets the actual toggle value to turn off rdp in the "allow rdp" menu
+        Disable-WindowsOptionalFeature -Online -FeatureName Remote-Desktop-Services #turns off the actual service
+        Stop-Service 'Remote Desktop Services' -Force -Confirm
+        Write-Output "Disabled RDP"
+    } catch {
+        Write-Output "Failed to disable RDP"
+    }
 }
