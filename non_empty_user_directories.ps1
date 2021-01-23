b90b4c1dd9f02b3fa883dbec65f562c6a43de8fa
David Bootle <d.bootle18@psaschool.org>
2021-01-23 15:10:58 -0500

    Create non_empty_user_directories.ps1

---
+++ non_empty_user_directories.ps1
@@ -0,0 +1,36 @@
+﻿$subdirectories = @(
+    "Desktop"
+    "Documents",
+    "Downloads",
+    "Music",
+    "Pictures",
+    "Saved Games",
+    "Videos"
+)
+
+Get-ChildItem -Directory -Path "C:\Users" | ForEach-Object -Process {
+    
+    $name = $_.Name
+
+    echo "$($name):"
+
+    $subdirectories | ForEach-Object -Process {
+
+
+        $folderpath = "C:\Users\$($name)\$($_)"
+
+        $folder = $_
+
+
+
+        try {
+            if ( (Get-ChildItem $folderpath -ErrorAction Stop | Measure-Object).Count -ne 0) {
+                echo "$($folder) is not empty"
+            }
+        } catch {
+            echo "$($folder) could not be accessed"
+        }
+    }
+
+    echo ""
+}
